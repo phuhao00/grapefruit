1dada0266d8e71efd89f4be9fac53b5f46688180
calvin <hhaou@outlook.com>
2023-11-07 19:14:31 +0800

    object pool demo add

--- internal/domain/do/login.go
+++ internal/domain/do/login.go
@@ -1,5 +1,7 @@
 package do
 
+import "sync"
+
 type LoginReq struct {
 	Name string `json:"name"`
 	Pwd  string `json:"pwd"`
@@ -17,4 +19,30 @@ type RegisterReq struct {
 }
 
 type RegisterRsp struct {
+}
+
+type ObjectPool struct {
+	pool sync.Pool
+}
+
+func NewObjectPool() *ObjectPool {
+	return &ObjectPool{
+		pool: sync.Pool{
+			New: func() interface{} {
+				return &LoginReq{} // 创建新的对象
+			},
+		},
+	}
+}
+
+func (p *ObjectPool) AcquireObject() *LoginReq {
+	obj := p.pool.Get() // 从对象池获取对象
+	if obj == nil {
+		return &LoginReq{} // 如果对象池为空，创建新的对象
+	}
+	return obj.(*LoginReq)
+}
+
+func (p *ObjectPool) ReleaseObject(obj *LoginReq) {
+	p.pool.Put(obj) // 将对象放回对象池
 }
